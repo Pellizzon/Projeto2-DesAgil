324c6ee3384809c028c8c6fb3a0b318172d74bd1
Pellizzon <38332600+Pellizzon@users.noreply.github.com>
2019-05-17 14:08:31 -0300

    App theme

--- app/src/main/java/br/edu/insper/al/matheusp1/projeto2/ui/login/ImpostoActivity.java
+++ app/src/main/java/br/edu/insper/al/matheusp1/projeto2/ui/login/ImpostoActivity.java
@@ -57,7 +57,6 @@ public class ImpostoActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.imposto, menu);
         return true;
     }
 
@@ -66,13 +65,6 @@ public class ImpostoActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
